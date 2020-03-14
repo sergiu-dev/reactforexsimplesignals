ff35a3fa4249856217b51431074097e1aa8d8828
sergiu-dev <sergiu.a.bugeac@gmail.com>
2020-03-14 10:58:54 +0200

    new mods done to nav

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './navigation.css'
 
+import { ReactComponent as Logos } from '../../assets/images/logo2.svg';
 import { ReactComponent as Logo } from '../../assets/images/logo.svg';
-import { ReactComponent as AllSignals } from '../../assets/images/chart.svg';
+import { ReactComponent as LogoArrow } from '../../assets/images/logo_arrow.svg';
+import { ReactComponent as AllSignals } from '../../assets/images/signals.svg';
 import { ReactComponent as About } from '../../assets/images/about.svg';
 import { ReactComponent as Contact } from '../../assets/images/envelope.svg';
 import { ReactComponent as Login } from '../../assets/images/login.svg';
@@ -11,11 +13,14 @@ import { ReactComponent as Login } from '../../assets/images/login.svg';
 const Navigation = () => (
 	<nav className='navigation'>
 		<ul className='navigation__menu'>
-			{/*<li className='navigation__item'>*/}
-			{/*	<Link className='navigation__link' to='/'>*/}
-			{/*		<Logo className='logo' />*/}
-			{/*	</Link>*/}
-			{/*</li>*/}
+			<li className='navigation__item-logo'>
+				<Logo className='navigation__logo' />
+			</li>
+			<li className='navigation__item-logo'>
+				<Link className='navigation__link' to='/'>
+					<LogoArrow className='navigation__link-icon' />
+				</Link>
+			</li>
 			<li className='navigation__item'>
 				<Link className='navigation__link' to='/all-signals'>
 					<AllSignals className='navigation__link-icon' />
