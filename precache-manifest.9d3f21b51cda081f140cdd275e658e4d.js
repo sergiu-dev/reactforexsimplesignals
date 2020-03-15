19704bff5c57cf2b79e8c84af31fe0abf4ee8d04
sergiu-dev <sergiu.a.bugeac@gmail.com>
2020-03-15 16:15:21 +0200

    Updates

---
+++ precache-manifest.9d3f21b51cda081f140cdd275e658e4d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbdf983eafa2d5ca15f3e00789a9804",
+    "url": "/reactforexsimplesignals/index.html"
+  },
+  {
+    "revision": "54be10a4db8db900a30f",
+    "url": "/reactforexsimplesignals/static/css/main.f135be5c.chunk.css"
+  },
+  {
+    "revision": "9ecd6e0729065bea36da",
+    "url": "/reactforexsimplesignals/static/js/2.af08d881.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reactforexsimplesignals/static/js/2.af08d881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54be10a4db8db900a30f",
+    "url": "/reactforexsimplesignals/static/js/main.22b708d4.chunk.js"
+  },
+  {
+    "revision": "e159e74db62a6fa10f4a",
+    "url": "/reactforexsimplesignals/static/js/runtime-main.edcec3b5.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/reactforexsimplesignals/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/reactforexsimplesignals/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "5b6d96cfe9abd593511f66e01f201d60",
+    "url": "/reactforexsimplesignals/static/media/about.5b6d96cf.svg"
+  },
+  {
+    "revision": "14dc4eba1d5528ca797549dcc65f455a",
+    "url": "/reactforexsimplesignals/static/media/arrow.14dc4eba.svg"
+  },
+  {
+    "revision": "1a9020c337be1b3a6556473307bb9a9f",
+    "url": "/reactforexsimplesignals/static/media/envelope.1a9020c3.svg"
+  },
+  {
+    "revision": "bafc506f372077c3f8536db285764d99",
+    "url": "/reactforexsimplesignals/static/media/header.bafc506f.jpg"
+  },
+  {
+    "revision": "fa2c07daf87d1ec998638f37e1412080",
+    "url": "/reactforexsimplesignals/static/media/home.fa2c07da.svg"
+  },
+  {
+    "revision": "f004e825dddcfc6a610b96cf1939daf0",
+    "url": "/reactforexsimplesignals/static/media/login.f004e825.svg"
+  },
+  {
+    "revision": "ff7b24789abd1e2f83874f217a4f5f39",
+    "url": "/reactforexsimplesignals/static/media/signals.ff7b2478.svg"
+  }
+]);
