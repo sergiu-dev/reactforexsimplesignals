f9a213b20aaf19078cb77389b81556065708e02b
sergiu-dev <sergiu.a.bugeac@gmail.com>
2020-03-15 11:06:50 +0200

    changed scroll to top into a class component

--- src/components/reusable/top_button.jsx
+++ src/components/reusable/top_button.jsx
@@ -2,35 +2,33 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 
-const TopButton = () => (
-	<button className='button button-top js-button-top hide'>
-		<FontAwesomeIcon icon={faChevronUp} />
-	</button>
-);
+class TopButton extends React.Component {
+	constructor(props) {
+		super(props);
 
-window.addEventListener('load', () => {
+		this.state = {
+			hasScrolled: false
+		}
+	}
 
-	/* ** Back to top button ** */
-
-	// Set a variable for our button element.
-	const scrollToTopButton = document.querySelector('.js-button-top');
+	componentDidMount() {
+		window.addEventListener('scroll', this.scrollFunc);
+	}
 
 	// Function that shows the scroll-to-top button if user scrolls beyond a certain value of the window height
-	const scrollFunc = () => {
+	scrollFunc = () => {
 		// Get the current scroll value
 		let y = window.scrollY;
-
+		console.log(y);
 		// If the scroll value is greater than the window height, will add the show class to the scroll-to-top button
 		if (y > 300) {
-			scrollToTopButton.className = 'button button-top js-button-top show';
+			this.setState({hasScrolled : true});
 		} else {
-			scrollToTopButton.className = 'button button-top js-button-top hide';
+			this.setState({hasScrolled : false});
 		}
 	};
 
-	window.addEventListener('scroll', scrollFunc);
-
-	const scrollToTop = () => {
+	scrollToTop = () => {
 		// Variable for the number of pixels we are from the top of the document
 		const top = document.documentElement.scrollTop || document.body.scrollTop;
 
@@ -38,18 +36,23 @@ window.addEventListener('load', () => {
 		// Animate that scroll with requestAnimationFrame:
 		// https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
 		if (top > 0) {
-			window.requestAnimationFrame(scrollToTop);
+			window.requestAnimationFrame(this.scrollToTop);
 			// ScrollTo takes an x and a y coordinate.
 			// Increase the '10' value to get a smoother/slower scroll!
 			window.scrollTo(0, top - top / 10);
 		}
 	};
 
-	// When the button is clicked, run ScrolltoTop function
-	scrollToTopButton.onclick = (e) => {
-		e.preventDefault();
-		scrollToTop();
-	};
-});
+	render () {
+		return (
+			<button
+				className={`${this.state.hasScrolled ? 'show' : 'hide'} button button-top js-button-top`}
+				onClick={this.scrollToTop}
+			>
+				<FontAwesomeIcon icon={faChevronUp} />
+			</button>
+		)
+	}
+}
 
 export default TopButton;
