5af497efbea1120a5e63ac9feee1738d1943b4e3
sergiu-dev <sergiu.a.bugeac@gmail.com>
2020-03-17 14:26:06 +0200

    added constants for routes

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const HOME = '/';
+export const ALL_SIGNALS = '/all-signals';
+export const ABOUT = '/about';
+export const CONTACT = '/contact';
+export const SIGN_IN = '/sign-in';
+export const PROFILE = '/profile';
+export const PASSWORD_FORGET = '/pw-forget';
